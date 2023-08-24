6e4df69d33628d62ec776dbccef71dec5d962714
smatzner <stefanmatzner@gmx.at>
2023-08-24 13:12:58 +0200

    added getTwData method

--- functions.php
+++ functions.php
@@ -6,17 +6,17 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class Guild
 {
-    public $name;
-    public $galacticPower;
-    public $memberCount;
-    public $avgGalacticPower;
-    public $avgArenaRank;
-    public $avgFleetArenaRank;
+    private $name;
+    private $galacticPower;
+    private $memberCount;
+    private $avgGalacticPower;
+    private $avgArenaRank;
+    private $avgFleetArenaRank;
 
     private $html;
     private $crawler;
 
-    public function __construct($guildID, $swgohRecruitmentID, $swgohRecruitmenttName = '')
+    public function __construct($guildID, $swgohRecruitmentID, $swgohRecruitmentName = '')
     {
         $response = file_get_contents('http://api.swgoh.gg/guild-profile/' . $guildID);
         $guildData = json_decode($response);
@@ -28,16 +28,46 @@ class Guild
         $this->avgArenaRank = floor($guildData->data->avg_arena_rank);
         $this->avgFleetArenaRank = floor($guildData->data->avg_fleet_arena_rank);
 
-        if(empty($swgohRecruitmenttName)){
-            $this->html = file_get_contents('https://recruit.swgoh.gg/guild/'. $swgohRecruitmentID . '/' . $this->name);
+        if (empty($swgohRecruitmentName)) {
+            $this->html = file_get_contents('https://recruit.swgoh.gg/guild/' . $swgohRecruitmentID . '/' . $this->name);
         } else {
-            $this->html = file_get_contents('https://recruit.swgoh.gg/guild/' . $swgohRecruitmentID . '/' . $swgohRecruitmenttName);
+            $this->html = file_get_contents('https://recruit.swgoh.gg/guild/' . $swgohRecruitmentID . '/' . $swgohRecruitmentName);
         }
 
         $this->crawler = new Crawler($this->html);
     }
 
-    public function getRaidData()
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getGalacticPower(): string
+    {
+        return $this->galacticPower;
+    }
+
+    public function getMemberCount()
+    {
+        return $this->memberCount;
+    }
+
+    public function getAvgGalacticPower(): string
+    {
+        return $this->avgGalacticPower;
+    }
+
+    public function getAvgArenaRank()
+    {
+        return $this->avgArenaRank;
+    }
+
+    public function getAvgFleetArenaRank()
+    {
+        return $this->avgFleetArenaRank;
+    }
+
+    public function getRaidData(): string
     {
         $raidProgressElement = $this->crawler->filter('h2:contains("Raid Progress") + div');
 
@@ -55,41 +85,54 @@ class Guild
 
         $raidData = '';
         foreach ($raidDataArray as $raidName => $raidScore) {
-            $raidData .= $raidScore . ' (' . $raidName . ')';
+            $raidData .= $raidScore . ' [' . $raidName . ']';
             if ($raidName != array_key_last($raidDataArray)) {
                 $raidData .= ' // ';
-            }   
+            }
         }
 
         return $raidData;
     }
 
-        public function getTBData()
-        {
-            $TBProgressElement = $this->crawler->filter('h2:contains("TB Progress") + div');
+    public function getTbData(): string
+    {
+        $tbProgressElement = $this->crawler->filter('h2:contains("TB Progress") + div');
+        $starImg = '<img src="https://www.swgoh.one/wp-content/uploads/2023/08/icon-star.png">';
 
-            $TBScores = [];
-            $TBProgressElement->children()->filter('div.card-body')->each(function ($TBScore) use (&$TBScores) {
-                array_push($TBScores, preg_replace('/[a-zA-Z]/', '', $TBScore->text()));
-            });
+        $tbScores = [];
+        $tbProgressElement->children()->filter('div.card-body')->each(function ($TBScore) use (&$tbScores) {
+            array_push($tbScores, preg_replace('/[a-zA-Z]/', '', $TBScore->text()));
+        });
 
-            $TBNames = [];
-            $TBProgressElement->children()->filter('img')->each(function ($TBName) use (&$TBNames) {
-                if($TBName->attr('alt') != 'star'){
-                    array_push($TBNames, strtoupper($TBName->attr('alt')));
-                }
-            });
-
-            $TBDataArray = array_combine($TBNames,$TBScores);
-            
-            $TBData = '';
-            foreach ($TBDataArray as $TBName => $TBScore) {
-                $TBData .= $TBScore . ' (' . $TBName . ')';
-                if ($TBName != array_key_last($TBDataArray)) {
-                    $TBData .= ' // ';
-                }
+        $tbNames = [];
+        $tbProgressElement->children()->filter('img')->each(function ($TBName) use (&$tbNames) {
+            if ($TBName->attr('alt') != 'star') {
+                array_push($tbNames, strtoupper($TBName->attr('alt')));
             }
+        });
 
-            return $TBData;
+        $tbDataArray = array_combine($tbNames, $tbScores);
+
+        $tbData = '';
+        foreach ($tbDataArray as $tbName => $tbScore) {
+            $tbData .= $tbScore . ' ' .$starImg. ' [' . $tbName . ']';
+            if ($tbName != array_key_last($tbDataArray)) {
+                $tbData .= ' // ';
+            }
         }
+
+        return $tbData;
     }
+
+    public function getTwData() : string
+    {
+        $badgeElements = $this->crawler->filter('.rounded-pill');
+
+        $badgeElements->each(function ($badgeElement) use (&$twBadge) {
+            if (preg_match('/.* TW$/', $badgeElement->text())) {
+                $twBadge = preg_replace('/\s+TW$/','',$badgeElement->text());
+            }
+        });
+        return $twBadge;
+    }
+}
